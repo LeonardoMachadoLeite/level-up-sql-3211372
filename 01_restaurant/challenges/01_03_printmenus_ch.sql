8a41c546acf6e0a1c3b39db8d58969e7d3cfffa4
Leonardo Machado Leite <38584731+LeonardoMachadoLeite@users.noreply.github.com>
2023-04-11 01:26:38 +0000

    First Study Session

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,17 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+select Name, Price, Type, Description --*
+from Dishes
+order by price;
+
 -- Create a report showing appetizers and beverages.
+select Name, Price, Type, Description --*
+from Dishes
+where Type in ('Appetizer', 'Beverage')
+order by Type;
+
 -- Create a report with all items except beverages.
+select Name, Price, Type, Description --*
+from Dishes
+where Type not in ('Beverage');
--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,9 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+insert into Customers (FirstName, LastName, Email, Address, City, State, Phone, Birthday, FavoriteDish)
+values ('Anna', 'Smith', 'asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '(555) 555-1212', '1973-07-21', 16);
+
+select *
+from Customers
+where Email = 'asmith@samoca.org';
